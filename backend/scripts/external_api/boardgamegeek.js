8a9c25b2f8c51fb7bfea18849c74399b7f2c93a6
lwiecek <lux22@interia.pl>
2017-07-16 23:03:17 +0200

    yargs for boardgamegeek

--- backend/scripts/external_api/boardgamegeek.js
+++ backend/scripts/external_api/boardgamegeek.js
@@ -2,3 +2,14 @@
 'use strict';
 
 const config = require('config');
+const argv = require('yargs')
+    .demandCommand(1)
+    .usage('Usage: $0 <command>')
+    .command('load_boardgames', 'Load all board games data from boardgamegeek xml api2')
+    .example('$0 load_boardgames', 'Load all board games data from boardgamegeek xml api2')
+    .help('help')
+    .alias('h', 'help')
+    .strict()
+    .argv;
+
+console.dir(argv);
