3ad59e0d19af0b8e413fc148bd7e028ff2327e6a
lwiecek <lux22@interia.pl>
2017-07-16 21:26:31 +0200

    publisher basic fields test

--- backend/test/publisher_test.js
+++ backend/test/publisher_test.js
@@ -2,6 +2,29 @@ const request = require('supertest');
 const app = require('../src/index').default;
 
 describe('publisher', () => {
+  test('basic fields work', () => {
+    return request(app).post('/graphql').send(
+      {
+        query: `{
+          publishers {
+            id
+            name
+            website_uri
+          }
+        }`
+      }
+    ).expect(200, {
+      'data': {
+        'publishers': [
+          {
+            "website_uri": "http://cheapass.com/",
+            "name": "Cheapass Games",
+            "id": "1"
+          }
+        ]
+      }
+    }).expect('Content-Type', 'application/json; charset=utf-8');
+  });
   test('nested fields work', () => {
     return request(app).post('/graphql').send(
       {
