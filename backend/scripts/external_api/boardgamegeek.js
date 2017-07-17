f5537491d6a0d08968983d598a9eb5b79a0ef700
lwiecek <lux22@interia.pl>
2017-07-18 00:37:24 +0200

    simple insert works

--- backend/scripts/external_api/boardgamegeek.js
+++ backend/scripts/external_api/boardgamegeek.js
@@ -4,6 +4,9 @@
 const config = require('config');
 const request = require('request');
 const xml2js = require('xml2js');
+const pg = require('pg');
+const SQL = require('sql-template-strings').SQL;
+
 const argv = require('yargs')
   .demandCommand(1)
   .usage('Usage: $0 <command>')
@@ -17,13 +20,50 @@ const argv = require('yargs')
 
 const command = argv._[0];
 
+const pgConfig = {
+  database: config.get('database.name'),
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+const pool = new pg.Pool(pgConfig);
+pool.on('error', err =>
+  console.error('idle client error', err.message, err.stack),
+);
+
 function createOrUpdateBoardGame(id) {
-  request(`${config.get('boardgamegeek.api_url')}/thing?type=boardgame&id=${id}`, (err, response, body) => {
+  const details = `type=boardgame&id=${id}&stats=1&videos=1`;
+  request(`${config.get('boardgamegeek.api_url')}/thing?${details}`, (err, response, body) => {
     if (err) {
       throw err;
     }
     xml2js.parseString(body, function (err, result) {
-      console.dir(result.items.item);
+      const item = result.items.item[0];
+      const value = (elm) => elm[0]['$'].value;
+      const name = value(item.name);
+      const slug = name;
+      const description = item.description[0];
+      const age_restriction = `[${value(item.minage)},]`;
+      const players_number = `[${value(item.minplayers)},${value(item.maxplayers)}]`;
+      const playing_time = `[${value(item.minplaytime)},${value(item.maxplaytime)}]`;
+      const query = SQL`
+        INSERT INTO boardgame(
+          name,
+          slug,
+          subtitle,
+          description,
+          age_restriction,
+          players_number,
+          playing_time
+        ) VALUES (
+          ${name},
+          ${slug},
+          '',
+          ${description},
+          ${age_restriction},
+          ${players_number},
+          ${playing_time}
+        );`;
+      return pool.query(query);
     });
   });
 }
