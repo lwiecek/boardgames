051bdebf25294c57ac08e9b0f4d2370eea469957
lwiecek <lux22@interia.pl>
2017-08-30 23:38:47 +0200

    fix test case

--- backend/test/scripts/external_api_bgg_test.js
+++ backend/test/scripts/external_api_bgg_test.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const bgg = require('../../scripts/external_api/bgg_util.js');
 
 describe('external api - boardgamegeek', () => {
-  test('board game upsert works', done => {
-    fs.readFile('fixtures/bgg_api_thing_boardgame.xml', (err, data) => {
-      expect(err).toBe(null);
-      const resolve = () => null;
-      const reject = () => null;
-      const bar = {tick: () => null};
-      bgg.parseBoardGameXML(3, bar, data, resolve, reject);
-      done();
-    });
+  test('board game upsert works', () => {
+    let ticks = 0;
+    return new Promise((resolve, reject) => {
+      fs.readFile('fixtures/bgg_api_thing_boardgame.xml', (err, data) => {
+        expect(err).toBe(null);
+        const bar = {tick: () => ++ticks};
+        bgg.parseBoardGameXML(3, bar, data, resolve, reject)
+      });
+    }).then(resolve => expect(ticks).toBe(1));
   });
 });
