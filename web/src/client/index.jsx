74141d471edb2f0cd2fbb459ffb1918bdcfc68c2
lwiecek <lux22@interia.pl>
2017-12-29 23:33:20 +0100

    react-router, server-side rendering, helmet

--- web/src/client/index.jsx
+++ web/src/client/index.jsx
@@ -5,21 +5,26 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Immutable from 'immutable';
+import { BrowserRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
-import App from './app';
-import helloReducer from './reducer/hello';
+import App from '../shared/app';
+import helloReducer from '../shared/reducer/hello';
 import { APP_CONTAINER_SELECTOR } from '../shared/config';
 import { isProd } from '../shared/util';
 
-// eslint-disable-next-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const preloadedState = window.__PRELOADED_STATE__;
+/* eslint-enable no-underscore-dangle */
 
 const store = createStore(
   combineReducers({ hello: helloReducer }),
+  { hello: Immutable.fromJS(preloadedState.hello) },
   composeEnhancers(applyMiddleware(thunkMiddleware)),
 );
 
@@ -30,9 +35,11 @@ if (!(rootEl instanceof Element)) {
 
 const wrapApp = (AppComponent, reduxStore) => (
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 );
 
@@ -40,9 +47,9 @@ ReactDOM.render(wrapApp(App, store), rootEl);
 
 if (module.hot) {
   // flow-disable-next-line
-  module.hot.accept('./app', () => {
+  module.hot.accept('../shared/app', () => {
     // eslint-disable-next-line global-require
-    const NextApp = require('./app').default;
+    const NextApp = require('../shared/app').default;
     ReactDOM.render(wrapApp(NextApp, store), rootEl);
   });
 }
